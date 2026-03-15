2b44a5c93c42d320a222f8f4b8a71190d304820b
Zack Gacnik <zgacnik@carroll.edu>
2026-03-14 21:08:51 -0600

    Basic text printing to page

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
-		})
+		c.String(http.StatusOK, "Hello, World!")
 	})
 
 	r.Run(":8080") // listens on http://localhost:8080
